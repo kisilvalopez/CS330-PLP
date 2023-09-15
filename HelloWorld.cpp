2603dd5f26ca0316190ffcc0dc04713239f30327
kisilvalopez <144376132+kisilvalopez@users.noreply.github.com>
2023-09-15 01:55:33 -0400

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World!";
+    
+    return 0;
+    //this is a comment
+    /*this is also a comment*/
+}
+
+//1. What is the name of your language? C++
+//2. When/where was it created, and by whom? Was it written to address a particular problem or need? C++ was created by Bjarne Stroustrup at Bell Laboratories in Murray Hill starting the period of 1979 when Stroustrup was doing work for his Ph.D thesis. He was using a language called Simula which was too slow and he began to work on “C with Classes”, wanting to add object-oriented programming into the C language. 
+//3. What types of programming is your language primarily used for (ex: web sites, video games, mobile devices, parsing, etc.)? If your language is multi-purpose, provide some examples of different projects it's been used for. It’s used for developing browsers, OS, applications, software engineering, data structures, in-game programming, and developing larged embedded systems.
+//4. Where will you get information about this language when it's time to start programming in it? At the end of your assignment, provide a list of the names of books, website URLs, or any other resources that relate to your language in particular.
+//https://www.w3schools.com/cpp/cpp_getstarted.asp 
+//https://code.visualstudio.com/docs/languages/cpp 
+//https://developers.google.com/edu/c++/getting-started 
+//https://github.com/arpit-omprakash/100ProjectsOfCode 
+
+//1. What did you need to do to install the language? 
+//To install the language I followed this youtube tutorial for Windows OS:
+//https://www.youtube.com/watch?v=uz7E--0nOVk&t=0s&ab_channel=TechDecodeTutorials 
+//I had to download MinGw and create an accessible path to it, then using the command prompt I made sure everything was downloaded, had VS Code download C++ for VS Code and Code Runner by Jun Han. I then made a C++ file. 
+//2. Does this language come with a recommended programming environment? What is it? If not, how did you pick the one that you'll be using? 
+//I picked VS Code because I already had it downloaded from previous classes.
+//3. How do you run programs in that language? 
+//Ctrl + alt + n
